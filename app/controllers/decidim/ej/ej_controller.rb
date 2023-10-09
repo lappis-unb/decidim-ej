0abe32761f7855f7390970c8b392109f6046b288
David Carlos <ddavidcarlos1392@gmail.com>
2023-10-09 19:19:38 -0300

    Initial integration with EJ api

--- app/controllers/decidim/ej/ej_controller.rb
+++ app/controllers/decidim/ej/ej_controller.rb
@@ -2,7 +2,14 @@ module Decidim
   module Ej
     class EjController < Decidim::Ej::ApplicationController
       def index
-        @data = {"comments_count": 4, "votes_count": 334}
+        ej_component = EjClient.find_by(component: params[:component_id])
+        client_api = ClientApi.new(current_user, ej_component)
+        begin
+          client_api.create_user
+        rescue
+          client_api.authenticate
+        end
+        @conversation = client_api.get_conversation()
       end
     end
   end
---
+++ app/models/decidim/ej/client_api.rb
@@ -0,0 +1,94 @@
+# frozen_string_literal: true
+require 'securerandom'
+require 'httparty'
+
+module Decidim
+  module Ej
+    class ClientApi
+      include HTTParty
+
+      attr_reader :new_user_route, :new_user_data, :token, :decidim_user_name, :decidim_user_email, :decidim_password, :user_data, :conversation_route, :conversation
+
+      def initialize(user, component)
+        @user = user
+        @host = component.host
+        @conversation_id = component.conversation_id
+        @routes = {
+          "users": "/api/v1/users/",
+          "login": "/api/v1/login/",
+          "conversations": "/api/v1/conversations/"
+        }
+      end
+
+      def login_route
+        return "#{@host}#{@routes[:login]}"
+      end
+
+      def new_user_route
+        return "#{@host}#{@routes[:users]}"
+      end
+
+      def conversation_route
+        return "#{@host}#{@routes[:conversations]}#{@conversation_id}"
+      end
+
+      def decidim_user_name
+        "decidim-#{@user.name}"
+      end
+
+      def decidim_user_email
+        "decidim-#{@user.email}"
+      end
+
+      def decidim_password
+        "decidim-#{@user.name}-#{@user.email}"
+      end
+
+      def new_user_data
+        password = SecureRandom.hex
+        {
+          "name": self.decidim_user_name,
+          "email": self.decidim_user_email,
+          "password": self.decidim_password,
+          "password_confirm": self.decidim_password
+        }
+      end
+
+      def user_data
+        {
+          "email": self.decidim_user_email,
+          "password": self.decidim_password
+        }
+      end
+
+      def get_conversation
+        response = self.class.get(self.conversation_route)
+        if response.code == 200
+          return JSON.parse response.body
+        else
+          raise "conversation not found"
+        end
+      end
+
+      def create_user
+        response = self.class.post(self.new_user_route, body: JSON.generate(self.new_user_data), headers: { 'Content-Type' => 'application/json' })
+        if response.code == 400
+          raise "user exists"
+        end
+        @token = response[:token]
+      end
+
+      def authenticate
+        response = self.class.post(self.login_route, body: JSON.generate(self.user_data), headers: { 'Content-Type' => 'application/json' })
+        if response.code == 200
+          @token = response[:token]
+        else
+          raise "error"
+        end
+      end
+
+      private
+
+    end
+  end
+end
