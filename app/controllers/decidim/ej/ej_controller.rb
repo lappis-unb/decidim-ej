4f1fd305b004b4c878416226a3e538595b4a8d89
Victor Jorge da Silva Gonçalves <victor.eng.unb@gmail.com>
2024-05-18 18:34:18 -0300

    feat: add ability to cache token and lazily authenticate user

--- app/controllers/decidim/ej/ej_controller.rb
+++ app/controllers/decidim/ej/ej_controller.rb
@@ -1,31 +1,23 @@
 module Decidim
   module Ej
     class EjController < Decidim::Ej::ApplicationController
-
       def index
-        ej_component = EjClient.find_by(component: params[:component_id])
-        client_api = ClientApi.new(current_user, ej_component)
-        begin
-          client_api.create_user
-        rescue
-          client_api.authenticate
-        end
-        @conversation = client_api.get_conversation()
-        @comment = client_api.get_next_comment()
+        @conversation = client_api.fetch_conversation
+        @comment = client_api.fetch_next_comment
       end
 
       def vote
-        ej_component = EjClient.find_by(component: params[:component_id])
-        client_api = ClientApi.new(current_user, ej_component)
-        begin
-          client_api.create_user
-        rescue
-          client_api.authenticate
-        end
-        @conversation = client_api.get_conversation()
-        client_api.vote(params[:choice], params[:comment_id])
-        @comment = client_api.get_next_comment()
+        @conversation = client_api.fetch_conversation
+        client_api.post_vote(params[:choice], params[:comment_id])
+        @comment = client_api.fetch_next_comment
+
         render partial: "decidim/ej/ej/component"
+      end
+
+      private
+
+      def client_api
+        @client_api ||= ClientApi.new(current_user, EjClient.find_by(component: params[:component_id]))
       end
     end
   end
--- app/models/decidim/ej/client_api.rb
+++ app/models/decidim/ej/client_api.rb
@@ -4,20 +4,33 @@ require 'httparty'
 
 module Decidim
   module Ej
+    class Unauthorized < StandardError
+    end
+
+    class Forbidden < StandardError
+    end
+
+    class UnknownError < StandardError
+    end
+
     class ClientApi
       include HTTParty
 
-      attr_reader :new_user_route, :new_user_data, :token, :decidim_user_name, :decidim_user_email, :decidim_password, :user_data, :conversation_route, :conversation, :headers
+      VOTE_OPTIONS = {
+        agree: 1,
+        disagree: -1,
+        skip: 0
+      }.freeze
 
       def initialize(user, component)
         @user = user
         @host = component.host
         @conversation_id = component.conversation_id
         @routes = {
-          "users": "/api/v1/users/",
-          "login": "/api/v1/login/",
-          "conversations": "/api/v1/conversations/",
-          "votes": "/api/v1/votes/"
+          users: "/api/v1/users/",
+          login: "/api/v1/login/",
+          conversations: "/api/v1/conversations/",
+          votes: "/api/v1/votes/"
         }
       end
 
@@ -33,7 +46,7 @@ module Decidim
         "#{@host}#{@routes[:conversations]}#{@conversation_id}"
       end
 
-      def comment_route(*args)
+      def comment_route
         "#{@host}#{@routes[:conversations]}#{@conversation_id}/random-comment"
       end
 
@@ -54,74 +67,145 @@ module Decidim
       end
 
       def new_user_data
-        password = SecureRandom.hex
         {
-          "name": self.decidim_user_name,
-          "email": self.decidim_user_email,
-          "password": self.decidim_password,
-          "password_confirm": self.decidim_password
+          name: decidim_user_name,
+          email: decidim_user_email,
+          password: decidim_password,
+          password_confirm: decidim_password
         }
       end
 
       def user_data
         {
-          "email": self.decidim_user_email,
-          "password": self.decidim_password
+          email: decidim_user_email,
+          password: decidim_password
         }
       end
 
-      def get_conversation
-        response = self.class.get(self.conversation_route)
-        if response.code == 200
-          return JSON.parse response.body
-        else
-          raise "conversation not found"
-        end
+      def fetch_conversation
+        fire_action_request(:conversation)
       end
+
+      def post_vote
+        fire_action_request(:vote)
+      end
+
+      def fetch_next_comment
+        fire_action_request(:next_comment)
+      end
+
+      def cached_token
+        Rails.cache.read(user_token_cache_identifier)
+      end
+
+      private
 
       def headers
-        {'Authorization': "Token #{@token}"}
+        { 'Authorization' => "Token #{cached_token}" }
       end
 
-      def vote(choice, comment_id)
-        votes_options = {"agree": 1, "disagree": -1, "skip": 0}
-        data = {"choice": votes_options[choice.to_sym], "comment": comment_id, "channel": "opinion_component"}
-        response = self.class.post(self.vote_route, body: data, headers: self.headers)
-        if not [200, 201].include? response.code
-          raise "vote could not be saved: #{response.parsed_response}"
+      def fire_action_request(action, *args, attempt: 0)
+        raise Decidim::Ej::UnknownError if attempt > 5
+
+        begin
+          authenticate
+          send(action, *args)
+        rescue Decidim::Ej::Forbidden
+          invalidate_cached_token!
+          fire_action_request(action, *args, attempt: attempt + 1)
+        rescue Decidim::Ej::Unauthorized
+          invalidate_user_registration!
+          fire_action_request(action, *args, attempt: attempt + 1)
         end
-      end
-
-      def get_next_comment
-          response = self.class.get(self.comment_route, headers: self.headers)
-          if response.code == 200
-            body = JSON.parse response.body
-            if not body["content"]
-              return {"content"=>"You have voted on all comments. Thank you for the participation."}
-            end
-            return body
-          else
-            raise "comment could not be retrieved"
-          end
-      end
-
-      def create_user
-        response = self.class.post(self.new_user_route, body: JSON.generate(self.new_user_data), headers: { 'Content-Type' => 'application/json' })
-        if response.code == 400
-          raise "user exists"
-        end
-        body = JSON.parse response.body
-        @token = body["token"]
       end
 
       def authenticate
-        response = self.class.post(self.login_route, body: JSON.generate(self.user_data), headers: { 'Content-Type' => 'application/json' })
-        if response.code == 200
-          body = JSON.parse response.body
-          @token = body["token"]
-        else
-          raise "error"
-        end
+        return create_user unless @user.has_ej_account?
+        return cached_token if cached_token.present?
+
+        response = self.class.post(
+          login_route,
+          body: JSON.generate(user_data),
+          headers: { 'Content-Type' => 'application/json' }
+        )
+
+        raise Unauthorized if response.code == 401
+        raise UnknownError unless response.code == 200
+
+        body = JSON.parse response.body
+
+        # TODO: Receive expiration time in the response and properly set here
+        set_cached_token(body["token"])
+      end
+
+      def vote(choice, comment_id)
+        request_body = {
+          choice: VOTE_OPTIONS[choice.to_sym],
+          comment: comment_id, channel:
+          "opinion_component"
+        }
+        response = self.class.post(vote_route, body: request_body, headers: headers)
+
+        raise Unauthorized if response.code == 401
+        raise Forbidden if response.code == 403
+        raise UnknownError unless response.code.in?([200, 201])
+
+        response
+      end
+
+      def conversation
+        response = self.class.get(conversation_route)
+
+        raise UnknownError unless response.code == 200
+
+        JSON.parse response.body
+      end
+
+      def next_comment
+        response = self.class.get(comment_route, headers: headers)
+
+        raise Unauthorized if response.code == 401
+        raise Forbidden if response.code == 403
+        raise UnknownError, "comment could not be retrieved" unless response.code == 200
+
+        body = JSON.parse response.body
+        body = { "content" => "You have voted on all comments. Thank you for the participation." } unless body["content"]
+
+        body
+      end
+
+      def set_cached_token(value, expiration: nil)
+        expiration = expiration.presence || 3600
+        Rails.cache.write(user_token_cache_identifier, value, expires_in: expiration.seconds)
+      end
+
+      def invalidate_cached_token!
+        Rails.cache.write(user_token_cache_identifier, nil)
+      end
+
+      def user_token_cache_identifier
+        "ej/users/#{@user.id}/token"
+      end
+
+      def create_user
+        response = self.class.post(
+          new_user_route,
+          body: JSON.generate(new_user_data),
+          headers: { 'Content-Type' => 'application/json' }
+        )
+
+        @user.update_column(:has_ej_account, true)
+
+        raise Decidim::Ej::Forbidden if response.code == 400
+
+        body = JSON.parse response.body
+
+        # TODO: Receive expiration time in the response and properly set here
+        set_cached_token(body["token"])
+      end
+
+      def invalidate_user_registration!
+        @user.update_column(:has_ej_account, false)
       end
     end
   end
